ecfdb9c074d206de601f1b7410d731cc217f1228
Hedieh Rafiee <hrafiee1995@gmail.com>
2022-10-04 10:32:27 +0330

    clean codes

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -3,11 +3,8 @@ import {
   ElementRef,
   HostBinding,
   Input,
-  OnChanges,
-  SecurityContext,
   SimpleChanges,
 } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Directive({
   selector: '[highlight]',
@@ -15,15 +12,15 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class HighlightDirective {
   @Input('highlight') searchTerm: string = '';
 
-  @HostBinding('innerHtml')
-  content: string = '';
-  constructor(private el: ElementRef, private sanitizer: DomSanitizer) {}
+  @HostBinding('innerHtml') content: string = '';
+  constructor(private el: ElementRef) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.el?.nativeElement) {
       if ('searchTerm' in changes) {
         const text = (this.el.nativeElement as HTMLElement)
           .textContent as string;
+
         if (this.searchTerm === '') {
           this.content = text as string;
         } else {
@@ -31,11 +28,8 @@ export class HighlightDirective {
           const newText = text.replace(regex, (match: string) => {
             return `<mark class="highlight highlighted-text">${match}</mark>`;
           });
-          const sanitzed = this.sanitizer.sanitize(
-            SecurityContext.HTML,
-            newText
-          );
-          this.content = sanitzed as string;
+
+          this.content = newText;
         }
       }
     }
